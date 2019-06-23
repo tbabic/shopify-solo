fd7596538a20c3ce36d384b5616ca1696c4acb23
tomislav babic <tomislav.babic.hr@gmail.com>
2019-06-23 15:36:56 +0200

    merging tenders and drafts controller

--- src/main/java/org/bytepoet/shopifysolo/controllers/TenderController.java
+++ src/main/java/org/bytepoet/shopifysolo/controllers/TenderController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.ContentCachingRequestWrapper;
 
-@RequestMapping("/tenders")
+@RequestMapping({"/tenders", "/drafts"})
 @RestController
 public class TenderController {
 	
