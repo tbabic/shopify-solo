0dcd52d41cbce6899c81698aa7558b33b4c947f8
tomislav babic <tomislav.babic.hr@gmail.com>
2021-01-25 14:09:39 +0100

    add timeout for okhttp client

--- src/main/java/org/bytepoet/shopifysolo/Application.java
+++ src/main/java/org/bytepoet/shopifysolo/Application.java
@@ -1,5 +1,7 @@
 package org.bytepoet.shopifysolo;
 
+import java.util.concurrent.TimeUnit;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -15,7 +17,11 @@ public class Application {
 	
 	@Bean
 	public OkHttpClient okHttpClient() {
-		return new OkHttpClient();
+		return new OkHttpClient.Builder()
+			    .connectTimeout(10, TimeUnit.SECONDS)
+			    .writeTimeout(10, TimeUnit.SECONDS)
+			    .readTimeout(60, TimeUnit.SECONDS)
+			    .build();
 	}
 	
 
