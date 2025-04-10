d358ff9ab5b3712b9c203fb67029e9f0c5d6b478
tbabic <tomislav.babic.hr@gmail.com>
2025-04-10 12:44:16 +0200

    box now fixes

--- src/main/java/org/bytepoet/shopifysolo/services/BoxNowService.java
+++ src/main/java/org/bytepoet/shopifysolo/services/BoxNowService.java
@@ -1,9 +1,15 @@
 package org.bytepoet.shopifysolo.services;
 
+import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.TreeMap;
 import java.util.stream.Collectors;
 
 import org.apache.commons.codec.binary.Base64;
@@ -13,9 +19,15 @@ import org.bytepoet.shopifysolo.manager.models.FileData;
 import org.bytepoet.shopifysolo.manager.models.Order;
 import org.bytepoet.shopifysolo.shopify.clients.ShopifyApiClient;
 import org.bytepoet.shopifysolo.shopify.models.ShopifyOrder;
+import org.bytepoet.shopifysolo.webinvoice.models.WebInvoiceDetails;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Service
 public class BoxNowService {
@@ -25,21 +37,47 @@ public class BoxNowService {
 	
 	@Autowired
 	private ShopifyApiClient shopifyApiClient;
+	
+	private static BoxNowLocationData locationData = null;
+	
+	private static Map<String, BoxNowLocation> locationsMap = new TreeMap<String, BoxNowLocation>();
+	
+	public static class BoxNowLocationData {
+		@JsonProperty
+		private List<BoxNowLocation> data;
+	}
+	
+	@JsonIgnoreProperties(ignoreUnknown = true)
+	public static class BoxNowLocation
+	{
+		@JsonProperty
+		private String title;
+		
+		@JsonProperty
+		private String id;
+		
+		@JsonProperty
+		private String name;
+	}
 
 	
 	public FileData generateAddressBookCsv(List<Order> orders) throws Exception {
 		
+		if (orders.isEmpty())
+		{
+			throw new RuntimeException("No orders selected");
+		}
 		List<String> shopifyOrderIds = orders.stream().map(o -> o.getShopifyOrderId()).collect(Collectors.toList());
 		List<ShopifyOrder> shopifyOrders = shopifyApiClient.getOrders(shopifyOrderIds, null, null, null, null);
-		validate(shopifyOrders);
+		//validate(shopifyOrders);
 		Map<String, ShopifyOrder> map = shopifyOrders.stream().collect(Collectors.toMap(o -> o.getId(), o -> o));
-		
+		CsvBuilder<Order> builder = new CsvBuilder<>();
 		byte [] bytes = new CsvBuilder<Order>()
-				.setEncoding("windows-1250")
+				.setEncoding("UTF-8")
 				.setDataObjects(orders)
 				
 				.addHeaderAndField("from_location", o -> location)
-				.addHeaderAndField("destination_location", o -> map.get(o.getShopifyOrderId()).getNoteAttribute("boxNowlockerId"))
+				.addHeaderAndField("destination_location", o -> this.getLocationId(map.get(o.getShopifyOrderId()).getShippingTitle()))
 				.addHeaderAndField("customer_phone_number", o -> o.getShippingInfo().getPhoneNumber())
 				.addHeaderAndField("customer", o -> o.getContact())
 				.addHeaderAndField("customer_full_name", o -> o.getShippingInfo().getFullName())
@@ -68,6 +106,52 @@ public class BoxNowService {
 		if (invalid) {
 			throw new RuntimeException(message);
 		}
+	}
+	
+	
+	private void loadFile() {
+		
+		
+		
+		ObjectMapper mapper = new ObjectMapper();
+		try {
+			BoxNowService.locationData = mapper.readValue(new ClassPathResource("apms_hr-HR.json").getURL(), BoxNowLocationData.class);
+			
+			for(BoxNowLocation location : BoxNowService.locationData.data) {
+				String key = location.title.trim() + ", " + location.name.trim();
+				key = key.replaceAll(" ,", ",");
+				if(locationsMap.containsKey(key))
+		        {
+		        	throw new RuntimeException("box now location key already exists: " + key);
+		        }
+				
+		        locationsMap.put(key, location);
+		        
+		    }
+			
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+		
+		
+
+		
+	}
+	
+
+	
+	private String getLocationId(String shippingString) {
+		if (locationsMap.isEmpty()) {
+			loadFile();
+		}
+		
+		String key = shippingString.replaceFirst("BOX NOW – ", "").replaceAll(" ,", ",").trim();
+		if (!locationsMap.containsKey(key))
+		{
+			throw new RuntimeException("No locker for: " + shippingString);
+		}
+		return locationsMap.get(key).id;
+		
 	}
 	
 	
